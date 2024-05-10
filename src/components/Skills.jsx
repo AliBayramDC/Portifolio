c69b106640d2b856b648b1f3ab72b5ac99375deb
AliBayramDC <121183289+AliBayramDC@users.noreply.github.com>
2024-05-10 09:04:14 +0400

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -39,6 +39,10 @@ const Skills = () => {
                     <img className='w-20 mx-auto' src={GitHub} alt='html icon' />
                     <p className='py-4'>Github</p>
                 </div>
+                <div className='shadow-md shadow-[#5c6199] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={GitHub} alt='html icon' />
+                    <p className='py-4'>Github</p>
+                </div>
             </div>
         </div>
     </div>
